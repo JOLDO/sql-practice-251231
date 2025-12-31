b03c670119b80dad155bc1b9357b1bfe4b26eafe
JOLDO <bbat115@gmail.com>
2025-12-31 16:51:12 +0900

    31일-실습2 마무리

--- 251231-db/1-ex1-초기-연습장.sql
+++ 251231-db/1-ex1-초기-연습장.sql
@@ -2,3 +2,14 @@ select * from emp WHERE JOB = 'MANAGER';
 select * from emp WHERE JOB = 'MANAGER' AND ENAME = 'JONES';
 select * from emp;
 select ENAME, SAL from emp WHERE SAL > 4999;
+
+-- 중볼값 제거
+SELECT DISTINCT JOB FROM EMP;
+
+-- ALL 생략된 상태
+SELECT JOB FROM EMP;
+
+-- 다중열 중복 제거
+SELECT DISTINCT JOB, DEPTNO FROM EMP;
+
+SELECT JOB, DEPTNO FROM EMP;
