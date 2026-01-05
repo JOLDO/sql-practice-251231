49e48d177f080ead4f6a478f93839300f387fc6e
JOLDO <bbat115@gmail.com>
2026-01-05 16:19:00 +0900

    260105 오후

--- 260105-db/2실습2.sql
+++ 260105-db/2실습2.sql
@@ -11,3 +11,32 @@ SELECT lower(ename) as ENAME, upper(job) as JOB from emp;
 
 -- emp 테이블에서 이름과 이름의 길이를 표시하되, 단 이름의 길이가 5이하인 것만 표시하시오.
 SELECT ename as 이름, length(ename) as "이름의 길이" from emp where length(ename) <= 5;
+
+SELECT length('한글'), lengthb('한글') from dual; -- UTF8은 한글을 3바이트씩 할당
+
+-- emp 테이블, 사원명 중에 's'가 포함된 사원들을 조회 하시오.
+SELECT * from emp where lower(ename) like '%s%';    -- like, not, or는 리소스 많이 먹음
+SELECT * from emp where ename like upper('%s%');
+SELECT * from emp where instr(ename, upper('s')) != 0;
+SELECT instr(ename, upper('s')) from emp where instr(ename, upper('s')) != 0;  -- 검증
+
+-- 010-1234-5678 -> 01012345678
+SELECT '010-1234-5678', replace('010-1234-5678', '-') as "전화번호" from dual;
+
+-- '01012345678' -> '1234', '5678'
+SELECT substr('01012345678', 4, 4) as "앞자리", substr('01012345678', -4) as "뒷자리" from dual;
+SELECT substr('01012345678', instr('01012345678', '010') + 3, 4) as "앞자리", substr('01012345678', -4) as "뒷자리" from dual;
+
+-- 010-1234-5678 -> '1234', '5678'
+SELECT substr('010-1234-5678', instr('010-1234-5678', '-', 1, 1) + 1, 4) as "앞자리", substr('010-1234-5678', instr('010-1234-5678', '-', 1, 2) + 1, 4) as "뒷자리" from dual; -- 이런식으로 쪼개도 됨
+
+SELECT '010-1234-****', '900110-1******'
+, rpad('010-1234-', 13, '*')
+, rpad('900110-1', 14, '*')
+from dual;
+
+SELECT '010-1234-5678', '900110-1111111'
+, rpad(substr('010-1234-5678', 1, instr('010-1234-5678', '-', 1, 2)), 13, '*')
+, rpad(substr('900110-1111111', 1, instr('900110-1111111', '-', 1, 1) + 1), 14, '*')
+, rpad(substr('010-1234-5678', 1, instr('010-1234-5678', '-', 1, 2)), length('010-1234-5678'), '*')
+from dual;
--- 260105-db/2오라클함수.sql
+++ 260105-db/2오라클함수.sql
@@ -1,6 +1,48 @@
 select ename,upper(ename) as 대문자이름, lower(ename) as 소문자이름, INITCAP(ename) as 첫글자 from emp; -- upper, lower, initcap
 select ename, length(ename) as 길이, lengthb(ename) as 바이트길이 from emp;   -- length, lengthb
+
+
+SELECT length('한글'), lengthb('한글') from dual; -- UTF8은 한글을 3바이트씩 할당
+
+
 select 1 + '1' from dual;   -- 사칙연산은 뒤에 문자라도 자동으로 숫자로 변경됨
 SELECT 1 + 'A' from dual;  -- 확실한 문자라면 에러가 뜸
 SELECT 1 || 'A' from dual;  -- 문자를 붙이려면 ||해주면 됨
 SELECT '1' || 'A' from dual;  -- 문자를 붙이려면 ||해주면 됨
+
+
+SELECT job, substr(job, 1, 2), substr(job, 3, 2), substr(job, 5, length(job) - 4), substr(job, 5), length(substr(job, 5, 10)) from emp;
+select job, substr(job, -3, 2) from emp;
+select job, substr(job, -length(job)) from emp; -- substr(글자 자르기)
+
+
+SELECT job, instr(job, 'K') from emp;   
+select instr('HELLO, ORACLE!', 'L', 5), instr('HELLO, ORACLE!', 'L', 2, 2), instr('HELLO, ORACLE!', ','), substr('HELLO, ORACLE!', 1, instr('HELLO, ORACLE!', ',') -1) from dual;   -- instr(글자 위치)
+SELECT ename, replace(ename, 'S', '!'), replace(ename, 'S') from emp;   -- replace
+
+
+select rpad(substr(ename, 1, 1), length(ename), '*') from emp;
+SELECT '010-1234', '900110-1'
+, lpad('12345', 10, '#')
+, rpad('12345', 10, '#')
+, lpad('12345', 3, '#')
+, rpad('12345', 3, '#')
+from dual;  -- lpad, rpad
+
+
+-- 사번:1111 성명:ㅇㅇㅇ
+SELECT concat(concat('사번:', empno), concat(' 성명:', ename)) from emp;    -- concat
+SELECT '사번:' || empno || ' 성명:' || ename from emp;
+
+
+SELECT '[' || ' _oracle_ ' || ']', '[' || trim(' _oracle_ ') || ']', '[' || ltrim(' _oracle_ ') || ']', '[' || rtrim(' _oracle_ ') || ']' from dual;    -- trim
+
+
+select 
+    sal / 3,
+    round(sal / 3, 2),
+    trunc(sal / 3, 2),
+    ceil(sal / 3),
+    floor(sal / 3),
+    mod(empno, 2)
+from emp;    -- round, trunc, ceil, floor, mod
